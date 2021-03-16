b4e34f4e253d0147ec788deb705536e16ae1e39e
Lutz Kadoch <LaMbdA2@gmx.at>
2021-03-16 12:26:08 +0100

    added loop for continuous dispensing

--- KaffeeMaschine/Program.cs
+++ KaffeeMaschine/Program.cs
@@ -9,41 +9,40 @@ namespace KaffeeMaschine
 
             Console.WriteLine("Hallo Nutzer, die Kaffeemaschine ist einsatzbereit!"); //Begrüssung ausgeben
 
-            //Kaffee/Tee Auswahlmenü anzeigen
-            Console.WriteLine("Kaffee -> Schöner schwarzer heisser kaffe, junge");
-            Console.WriteLine("Tee    -> Schwarzer Tee aus biologischem Anbau");
-            Console.WriteLine("Wasser -> Heisses Wasser");
-            Console.WriteLine("Nix    -> Falls sie Geringverdiener sind, Aus dem Weg!");
-
-            string userInput;
-            userInput = Console.ReadLine(); //Nutzereingabe einlesen
-
-            switch (userInput)
+            bool keepRunning = true;
+            do
             {
-                case "Tee":
-                    Console.WriteLine("Tee wird ausgegeben"); //Getränk ausgeben
-                    break;
-                case "Wasser":
-                    Console.WriteLine("Wasser wird ausgegeben"); //Getränk ausgeben
-                    break;
-                case "Kaffee":
-                    Console.WriteLine("Kaffee wird ausgegeben"); //Getränk ausgeben
-                    break;
-                case "Nix":
-                    Console.WriteLine("dann bekommen sie eben nix"); //Getränk ausgeben
-                    break;
-                default:
-                    Console.WriteLine("Die Eingabe kenne ich nicht"); // fehler
-                    break;
-            }
 
-            /*
-                if  zum testen
-                switch zum testen
-                Console.WriteLine(); für ausgaben
-                Console.ReadKey(); einen tastendruck
-                Console.ReadLine(); zeichen lesen bis zu einem Enter;
-            */
+                //Kaffee/Tee Auswahlmenü anzeigen
+                Console.WriteLine("Kaffee -> Schöner schwarzer heisser kaffe, junge");
+                Console.WriteLine("Tee    -> Schwarzer Tee aus biologischem Anbau");
+                Console.WriteLine("Wasser -> Heisses Wasser");
+                Console.WriteLine("Abschalten -> Schaltet die Maschine aus");
+
+                string userInput;
+                userInput = Console.ReadLine(); //Nutzereingabe einlesen
+
+                switch (userInput)
+                {
+                    case "Tee":
+                        Console.WriteLine("Tee wird ausgegeben"); //Getränk ausgeben
+                        break;
+                    case "Wasser":
+                        Console.WriteLine("Wasser wird ausgegeben"); //Getränk ausgeben
+                        break;
+                    case "Kaffee":
+                        Console.WriteLine("Kaffee wird ausgegeben"); //Getränk ausgeben
+                        break;
+                    case "Abschalten":
+                        Console.WriteLine("dann bekommen sie eben nix"); //Getränk ausgeben
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Die Eingabe kenne ich nicht"); // fehler
+                        break;
+                }
+            } while (keepRunning);
+
             Console.WriteLine("Kaffeemaschine beendet sich");
         }
     }
