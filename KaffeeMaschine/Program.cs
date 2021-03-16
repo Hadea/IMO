082f5dd03961689164f44465c9ce112820b0ea80
Lutz Kadoch <LaMbdA2@gmx.at>
2021-03-16 10:37:54 +0100

    Added coffee project

---
+++ KaffeeMaschine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KaffeeMaschine
+{
+    class Program
+    {
+        static void Main(string[] args) //               doppelklick auf exe
+        {
+            /*
+                Begrüssung ausgeben
+                Kaffee/Tee Auswahlmenü anzeigen
+                Nutzereingabe einlesen
+                Getränk ausgeben
+             */
+        }
+    }
+}
