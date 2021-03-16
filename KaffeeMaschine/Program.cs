1a2d99c7313e1c3277c644557efc5bbfb292d1e6
Lutz Kadoch <LaMbdA2@gmx.at>
2021-03-16 10:57:32 +0100

    added hints to commands

--- KaffeeMaschine/Program.cs
+++ KaffeeMaschine/Program.cs
@@ -12,6 +12,14 @@ namespace KaffeeMaschine
                 Nutzereingabe einlesen
                 Getränk ausgeben
              */
+
+            /*
+                if  zum testen
+                switch zum testen
+                Console.WriteLine(); für ausgaben
+                Console.ReadKey(); einen tastendruck
+                Console.ReadLine(); zeichen lesen bis zu einem Enter;
+            */
         }
     }
 }
