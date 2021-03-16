2fab55b36b99cc5864122ba24f2ed8df489ac3b1
Lutz Kadoch <LaMbdA2@gmx.at>
2021-03-16 11:48:55 +0100

    implemented first version of coffee mashine

--- KaffeeMaschine/Program.cs
+++ KaffeeMaschine/Program.cs
@@ -6,12 +6,36 @@ namespace KaffeeMaschine
     {
         static void Main(string[] args) //               doppelklick auf exe
         {
-            /*
-                Begrüssung ausgeben
-                Kaffee/Tee Auswahlmenü anzeigen
-                Nutzereingabe einlesen
-                Getränk ausgeben
-             */
+
+            Console.WriteLine("Hallo Nutzer, die Kaffeemaschine ist einsatzbereit!"); //Begrüssung ausgeben
+
+            //Kaffee/Tee Auswahlmenü anzeigen
+            Console.WriteLine("Kaffee -> Schöner schwarzer heisser kaffe, junge");
+            Console.WriteLine("Tee    -> Schwarzer Tee aus biologischem Anbau");
+            Console.WriteLine("Wasser -> Heisses Wasser");
+            Console.WriteLine("Nix    -> Falls sie Geringverdiener sind, Aus dem Weg!");
+
+            string userInput;
+            userInput = Console.ReadLine(); //Nutzereingabe einlesen
+
+            switch (userInput)
+            {
+                case "Tee":
+                    Console.WriteLine("Tee wird ausgegeben"); //Getränk ausgeben
+                    break;
+                case "Wasser":
+                    Console.WriteLine("Wasser wird ausgegeben"); //Getränk ausgeben
+                    break;
+                case "Kaffee":
+                    Console.WriteLine("Kaffee wird ausgegeben"); //Getränk ausgeben
+                    break;
+                case "Nix":
+                    Console.WriteLine("dann bekommen sie eben nix"); //Getränk ausgeben
+                    break;
+                default:
+                    Console.WriteLine("Die Eingabe kenne ich nicht"); // fehler
+                    break;
+            }
 
             /*
                 if  zum testen
@@ -20,6 +44,7 @@ namespace KaffeeMaschine
                 Console.ReadKey(); einen tastendruck
                 Console.ReadLine(); zeichen lesen bis zu einem Enter;
             */
+            Console.WriteLine("Kaffeemaschine beendet sich");
         }
     }
 }
