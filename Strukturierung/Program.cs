028c9d59ca803350e5557f3069c1815590931ea7
Lutz Kadoch <LaMbdA2@gmx.at>
2021-03-17 10:40:28 +0100

    added questions

--- Strukturierung/Program.cs
+++ Strukturierung/Program.cs
@@ -10,6 +10,17 @@ namespace Strukturierung
             int B = 3;
 
             int Ergebnis = Addition(A, B);
+
+
+            // Methode die 3 Zahlen entgegennimmt und deren Produkt zurückgibt
+            // übergabe von  2 4 2, rückgabe 16
+
+            // Methode die zwei zahlen entgegennimmt und alle zahlen zwischen diesen ausgibt
+            // übergabe von 2 7, ausgabe 3 4 5 6
+
+            // Methode die eine Zahl entgegennimmt und entsprechend der zahl fibunacci-werte ausgibt
+            // übergabe von 5, ausgabe 1 1 2 3 5
+
         }
 
         static int Addition(int ersterWert, int zweiterWert)
