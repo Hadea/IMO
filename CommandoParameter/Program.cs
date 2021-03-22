875b4fc918bc189a47cf93b3e7fff003d1581929
Lutz Kadoch <LaMbdA2@gmx.at>
2021-03-22 08:59:38 +0100

    added example for parameter

---
+++ CommandoParameter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CommandoParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Die Parameter sind:");
+
+            foreach (string item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
